4af1a0a0802a8c3e8292aba395011831bbb59f1f
StrathCole <strathcole@gmail.com>
2025-05-09 09:35:12 +0200

    test

--- pebble.go
+++ pebble.go
@@ -10,6 +10,7 @@ import (
 	"runtime"
 
 	"github.com/cockroachdb/pebble"
+	"github.com/cockroachdb/pebble/bloom"
 )
 
 func init() {
@@ -38,16 +39,18 @@ func NewPebbleDB(name string, dir string) (*PebbleDB, error) {
 		L0CompactionThreshold:       16,
 		L0StopWritesThreshold:       32,
 		MaxConcurrentCompactions:    func() int { return runtime.GOMAXPROCS(0) },
-		// Filters disabled for troubleshooting
-		BytesPerSync: 1 << 20, // 1MB
+		Filters: map[string]pebble.FilterPolicy{
+			"bloom": bloom.FilterPolicy(10), // 10 bits per key for bloom filter
+		},
+		//BytesPerSync: 1 << 20, // 1MB
 		Levels: []pebble.LevelOptions{
-			{TargetFileSize: 64 << 20, Compression: pebble.SnappyCompression},  // Level 0: 64MB
-			{TargetFileSize: 64 << 20, Compression: pebble.SnappyCompression},  // Level 1: 64MB
-			{TargetFileSize: 128 << 20, Compression: pebble.SnappyCompression}, // Level 2: 128MB
-			{TargetFileSize: 256 << 20, Compression: pebble.SnappyCompression}, // Level 3: 256MB
-			{TargetFileSize: 512 << 20, Compression: pebble.SnappyCompression}, // Level 4: 512MB
-			{TargetFileSize: 1 << 30, Compression: pebble.SnappyCompression},   // Level 5: 1GB
-			{TargetFileSize: 2 << 30, Compression: pebble.SnappyCompression},   // Level 6: 2GB
+			{TargetFileSize: 64 << 20, Compression: pebble.ZstdCompression},  // Level 0: 64MB
+			{TargetFileSize: 64 << 20, Compression: pebble.ZstdCompression},  // Level 1: 64MB
+			{TargetFileSize: 128 << 20, Compression: pebble.ZstdCompression}, // Level 2: 128MB
+			{TargetFileSize: 256 << 20, Compression: pebble.ZstdCompression}, // Level 3: 256MB
+			{TargetFileSize: 512 << 20, Compression: pebble.ZstdCompression}, // Level 4: 512MB
+			{TargetFileSize: 1 << 30, Compression: pebble.ZstdCompression},   // Level 5: 1GB
+			{TargetFileSize: 2 << 30, Compression: pebble.ZstdCompression},   // Level 6: 2GB
 		},
 	}
 	opts.EnsureDefaults()
