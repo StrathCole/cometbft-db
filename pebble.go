20487acf73fee265565b94ea5bfcc8c70a97018a
StrathCole <strathcole@gmail.com>
2025-05-09 09:42:04 +0200

    test

--- pebble.go
+++ pebble.go
@@ -31,8 +31,8 @@ func NewPebbleDB(name string, dir string) (*PebbleDB, error) {
 	cache := pebble.NewCache(16 << 30) // 16GB
 
 	opts := &pebble.Options{
-		Cache:                       cache,
-		MemTableSize:                256 << 20, // 256MB
+		Cache: cache,
+		//MemTableSize:                256 << 20, // 256MB
 		MemTableStopWritesThreshold: 4,
 		MaxOpenFiles:                50000,
 		L0CompactionThreshold:       16,
